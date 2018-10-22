e06175d38e4a867c2d3754fc3578f062d7fa0da8
Bailey Miller <bmiller@susquehannasoftware.com>
2018-10-22 19:18:59 -0400

    Trying to get the login database setup.

---
+++ LoginAPI/LoginAPI/Authentication/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAPI.Authentication
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
